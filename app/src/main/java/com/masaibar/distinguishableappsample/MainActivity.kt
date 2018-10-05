71a0950c2611028f27f06dfb0196d58d2464103d
masaibar <masaibar.dev@gmail.com>
2018-10-05 11:40:57 +0900

    TextViewにバージョン名を表示する

--- app/src/main/java/com/masaibar/distinguishableappsample/MainActivity.kt
+++ app/src/main/java/com/masaibar/distinguishableappsample/MainActivity.kt
@@ -2,11 +2,14 @@ package com.masaibar.distinguishableappsample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        text_version_name.text = BuildConfig.VERSION_NAME
     }
 }
